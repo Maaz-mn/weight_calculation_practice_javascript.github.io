a2d9b18b4c7530bb9de3d60e9adf5e8b117f3f39
Maaz Nadeem <maaznadeem007@outlook.com>
2024-02-16 07:54:58 +0500

    js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ var weightInGrams = prompt("Enter the weight of chicken in grams:");
 var weightInKg = weightInGrams / 1000;
 var price = weightInKg*500;
 // price of per KG is 500rs
-document.write("You have to pay "+ price + "rs for" + weightInGrams )
+document.write("You have to pay "+ price + "rs for " + weightInGrams + " grams of chicken. " )
